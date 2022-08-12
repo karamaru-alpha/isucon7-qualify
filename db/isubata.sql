1d004a7cd0aa05fc823d0d23dbfcc7d705c3c669
karamaru-alpha <mrnk3078@gmail.com>
2022-08-12 20:01:02 +0900

    a

--- db/isubata.sql
+++ db/isubata.sql
@@ -11,30 +11,24 @@ CREATE TABLE user (
 CREATE TABLE image (
   id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(191),
-  data LONGBLOB,
-  INDEX (`name`)
+  data LONGBLOB
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE channel (
   id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name TEXT NOT NULL,
   description MEDIUMTEXT,
-  message_cnt INT UNSIGNED NOT NULL DEFAULT 0,
   updated_at DATETIME NOT NULL,
   created_at DATETIME NOT NULL
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
-CREATE TRIGGER tr1 BEFORE INSERT ON `message` FOR EACH ROW UPDATE `channel` SET `message_cnt`=`message_cnt`+1  WHERE id = NEW.message.channel_id;
-CREATE TRIGGER tr2 BEFORE DELETE ON `message` FOR EACH ROW UPDATE `channel` SET `message_cnt`=`message_cnt`-1 WHERE id = OLD.message.channel_id;
 
 CREATE TABLE message (
   id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   channel_id BIGINT,
   user_id BIGINT,
   content TEXT,
-  created_at DATETIME NOT NULL,
-  INDEX (`channel_id`)
+  created_at DATETIME NOT NULL
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE haveread (
@@ -45,3 +39,7 @@ CREATE TABLE haveread (
   created_at DATETIME NOT NULL,
   PRIMARY KEY(user_id, channel_id)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `message` ADD INDEX (`channel_id`);
+ALTER TABLE `channel` ADD COLUMN message_cnt INT UNSIGNED NOT NULL DEFAULT 0;
+CREATE TRIGGER tr1 BEFORE INSERT ON `message` FOR EACH ROW UPDATE `channel` SET `message_cnt`=`message_cnt`+1 WHERE id = NEW.message.channel_id;
